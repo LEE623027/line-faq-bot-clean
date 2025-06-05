d4ca6a5d50653380ae502fe5c23ca37220dcd9df
unknown <yk0574@yueki.com.tw>
2025-06-05 19:28:21 +0800

    修正環境變數讀取

--- config.py
+++ config.py
@@ -2,4 +2,3 @@ import os
 
 CHANNEL_SECRET = os.environ.get("ca1b9decb4235aa49902d26bd8258022", "")
 CHANNEL_ACCESS_TOKEN = os.environ.get("qJmXLp0ViDZt8ePiuVEuZDyvcs/FpXcN6kzKE2WjnijniXEhhkau1n4N7e2JOx3EJCODGO5rkw1JJbYwaKgfUPHDrp8hAGsNXC28yTTuTuJxwez2CL9jAF7vUyAjNFBiIoWFZ/dzWAI+U3UetRpeMQdB04t89/1O/w1cDnyilFU=", "")
-
